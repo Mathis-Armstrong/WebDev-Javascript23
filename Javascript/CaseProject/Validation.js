98c3389646130cb4a5cc3958ff2de0a2e8a43f23
Mathis-Armstrong <mathis.armstrong99@gmail.com>
2023-04-03 20:21:44 -0500

    update

--- Javascript/CaseProject/Validation.js
+++ Javascript/CaseProject/Validation.js
@@ -1,9 +1,52 @@
 let check = document.getElementById("check");
 let check2 = document.getElementById("check2");
 let check3 = document.getElementById("check3");
+let check4 = document.getElementById("check4");
+let check5 = document.getElementById("check5");
 let radio = document.getElementById("radio");
 let text = document.getElementById("text");
 let submit = document.getElementById("submit");
+let selections = [];
+check.addEventListener("click", function() {
+    if(check.checked){
+        selections.push("1");
+    }
+    else{
+        selections.splice(selections.indexOf("1"),1);
+    }
+});
+check2.addEventListener("click", function() {
+    if(check2.checked){
+        selections.push("2");
+    }
+    else{
+        selections.splice(selections.indexOf("2"),1);
+    }
+});
+check3.addEventListener("click", function() {
+    if(check3.checked){
+        selections.push("3");
+    }
+    else{
+        selections.splice(selections.indexOf("3"),1);
+    }
+});
+check4.addEventListener("click", function() {
+    if(check4.checked){
+        selections.push("4");
+    }
+    else{
+        selections.splice(selections.indexOf("4"),1);
+    }
+});
+check5.addEventListener("click", function() {
+    if(check5.checked){
+        selections.push("5");
+    }
+    else{
+        selections.splice(selections.indexOf("5"),1);
+    }
+});
 
 submit.addEventListener("click", function(){
     if(!check.checked&&!check2.checked&&!check3.checked){
@@ -23,4 +66,5 @@ submit.addEventListener("click", function(){
         radio.setCustomValidity("");
         text.setCustomValidity("");
     }
+    document.getElementById("result").innerHTML = "You picked: "+selections;
 });
